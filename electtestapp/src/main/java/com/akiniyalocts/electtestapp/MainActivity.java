25b4291cf1712393b694087852fce084c61671f8
akiniyalocts <kiniyaloca1@gmail.com>
2016-01-27 22:54:46 -0500

    added empty testapp

---
+++ electtestapp/src/main/java/com/akiniyalocts/electtestapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.akiniyalocts.electtestapp;
+
+import android.os.Bundle;
+
+import com.akiniyalocts.elect.ElectActivity;
+import com.akiniyalocts.elect.electives.HasHomeAsUp;
+
+public class MainActivity extends ElectActivity implements HasHomeAsUp{
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
